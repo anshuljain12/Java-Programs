27229cd75b5f10ae403ae6e0b5613b08e32da395
Anshul Jain <anshul.jain1207@gmail.com>
2015-03-16 22:14:19 -0700

    Update RotateArray.java

--- LeetCodeOJ/src/Arrays/RotateArray.java
+++ LeetCodeOJ/src/Arrays/RotateArray.java
@@ -7,36 +7,38 @@ public class RotateArray {
 	 */
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		int[] nums = {1,2,3,4,5,6,7,8};
+		int[] nums = {1,2,3,4,5,6,7,8,9, 10, 11, 12};
 		for(int i=0;i<nums.length;i++)
         	System.out.print(nums[i]+" ");
 		System.out.println();
-		rotate(nums, 3);
+		rightRotate(nums, 3);
 		for(int i=0;i<nums.length;i++)
         	System.out.print(nums[i]+" ");
 	}
-	public static void rotate(int[] nums, int k) {
-        int len = nums.length;
-        int j=0;
-        int c=0;
-        int i = 0;
-        int m = k;
-        for(i=len-k;i<len;i++,j++,k++){
-        	int a = nums[j];
-        	nums[j] = nums[i];
-        	int b = nums[k];
-        	c = nums[k];
-        	nums[k] = a;
-        	nums[i] = c;
-        }
-        for(int l=0;l<nums.length;l++)
-        	System.out.print(nums[l]+" ");
-		System.out.println();
-		
-        for(i=len-m;i<len && k<len;i++,j++,k++){
-        	int a = nums[i];
-        	nums[i] = nums[k];
-        	nums[k] = a;
-        }
-    }
+	public static void rightRotate(int[] nums, int d) {
+		int i, j, k, temp;
+		int n = nums.length;
+		for (i = 0; i < n-gcd(d,n); i++)
+		{
+			temp = nums[i];
+			j = i;
+			while(true)
+			{
+				k = j + d;
+				if (k >= n)
+					k = k - n;
+				if (k == i)
+					break;
+				nums[j] = nums[k];
+				j = k;
+			}
+			nums[j] = temp;
+		}
+    	}
+	public static int gcd(int a, int b){
+		if(b==0)
+		     return a;
+		else
+		     return gcd(b, a%b);
+	}
 }
